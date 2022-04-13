1072aedff4ae6dbf94b5d6ff2b95d38909df4147
counterpadpi <lapins@crafteke.be>
2022-04-13 12:51:48 +0100

    added logic and disabling pad

--- controller.py
+++ controller.py
@@ -35,12 +35,13 @@ CANCEL_PAD=11
 ROOMS_LIGHTS_PAD=[2,6,10,14]
 ACTIVE_LIGHT_PAD= 15
 
+enabled_pad=False
 # Turn on every LED
 def allLedsOn():
     print("Turning all LEDs on...")
     trellis.led.fill(True)
-    trellis.led[CANCEL_PAD] = False
-    trellis.led[VALIDATION_PAD] = False
+    #trellis.led[CANCEL_PAD] = False
+    #trellis.led[VALIDATION_PAD] = False
 
 
 
@@ -49,26 +50,18 @@ def allLedsOn():
 def allLedsOff():
     print("Turning all LEDs off...")
     trellis.led.fill(False)
-    trellis.led[CANCEL_PAD] = True
-    trellis.led[VALIDATION_PAD] = True
+    #trellis.led[CANCEL_PAD] = True
+    #trellis.led[VALIDATION_PAD] = True
 
 
-def switchPadLed(state):
+def switchPadLed(state,toggle_control=False):
     for i,val in enumerate(PAD_MAPPING):
         if val != 42:
             trellis.led[i]=state
+    if(toggle_control):
+        trellis.led[CANCEL_PAD] = state
+        trellis.led[VALIDATION_PAD] = state
 
-# Turn on every LED, one at a time
-def animationStart():
-    print("Turning on each LED, one at a time...")
-    for i in range(32):
-        trellis.led[i] = True
-        time.sleep(0.05)
-    # Turn off every LED, one at a time
-    print("Turning off each LED, one at a time...")
-    for i in range(31, 0, -1):
-        trellis.led[i] = False
-        time.sleep(0.05)
 
 def resetCode():
     global input_code
@@ -87,12 +80,12 @@ def initTrellis():
     trellis.led[CANCEL_PAD] = True
     pressed_buttons = set()
     trellis.read_buttons()
-    allLedsOff()
     time.sleep(2) # to avoid init noise
+    allLedsOff()
+
 
 def monitorButtons():
     global pressed_buttons
-    time.sleep(0.05)
     just_pressed, released = trellis.read_buttons()
     msg={}
     for b in just_pressed:
@@ -118,14 +111,11 @@ def monitorButtons():
             time.sleep(1)
             switchPadLed(False)
             trellis.led[CANCEL_PAD] = True
-            trellis.led[VALIDATION_PAD] = True            
+            trellis.led[VALIDATION_PAD] = True
         else:
             index=PAD_MAPPING[b]
             if(index<len(input_code)):
-                if(input_code[index]): #check if button was on or off
-                    trellis.led[b] = False
-                else:
-                    trellis.led[b] = True
+                trellis.led[b]=input_code[index]
                 input_code[index] = not input_code[index]
 
     pressed_buttons.update(just_pressed)
@@ -141,8 +131,8 @@ def monitorButtons():
         #trellis.led[b] = True
 @sio.event
 def Command(data):
-    global running_countdown,remaining_time
-    print(data)
+    global running_countdown,remaining_time,enabled_pad
+    #print(data)
     if(data['controller_id']=='start_counter'):
         running_countdown=True
         remaining_time=2*int(data['value'])
@@ -150,6 +140,18 @@ def Command(data):
     if(data['controller_id']=='pause_counter'):
         running_countdown=False
         print("Pausing counter.")
+    if(data['controller_id']=='trigger_pad_state'):
+        if(int(data['value'])==1):
+            enabled_pad = True
+            trellis.led[CANCEL_PAD] = True
+            trellis.led[VALIDATION_PAD] = True
+            resetCode()
+            print("Enable pad.")
+        else:
+            enabled_pad = False
+            switchPadLed(False,True)
+            resetCode()
+            print("Disable pad.")
     if(data['controller_id']=='corridor_room_on'):
         trellis.led[ROOMS_LIGHTS_PAD[int(data['value'])-1]] = True
         print("Switch room ", data['value']," led on.")
@@ -181,6 +183,7 @@ def final_countDown():
         #     print( "Remaining time : ",remaining_time/2)
 
 def main():
+    global enabled_pad
     print("Starting corridor controller...")
     try:
         sio.connect(SIO_SERVER)
@@ -190,15 +193,22 @@ def main():
         sio.emit('Register',"counterpadPi")
         print("Connected with SID ",sio.sid)
     tl.start(block=False)
-    animationStart()
+    #animationStart()
     print("Blinking counter.")
     countdown.test_led()
+    allLedsOn()
+    time.sleep(0.5)
+    allLedsOff() # for trellis
+    time.sleep(0.5)
+    allLedsOn()
     initTrellis()
     while True:
         try:
-            monitorButtons()
+            if enabled_pad:
+                monitorButtons()
         except OSError:
             print("Lost connection to I2C trellis. Trying back...")
+        time.sleep(0.05)
 
 def handler(signal_received, frame):
     # Handle any cleanup here
@@ -210,3 +220,15 @@ def handler(signal_received, frame):
 if __name__ == "__main__":
     signal(SIGINT, handler)
     main()
+
+# Turn on every LED, one at a time
+def animationStart():
+    print("Turning on each LED, one at a time...")
+    for i in range(32):
+        trellis.led[i] = True
+        time.sleep(0.05)
+    # Turn off every LED, one at a time
+    print("Turning off each LED, one at a time...")
+    for i in range(31, 0, -1):
+        trellis.led[i] = False
+        time.sleep(0.05)
